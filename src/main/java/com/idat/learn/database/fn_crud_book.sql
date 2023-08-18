2b75fe48571be809f4964f3bbe17db720345ed95
David <davidcastroastudillo20@gmail.com>
2023-08-17 19:27:28 -0500

    ---

--- src/main/java/com/idat/learn/database/fn_crud_book.sql
+++ src/main/java/com/idat/learn/database/fn_crud_book.sql
@@ -1,47 +1,29 @@
-CREATE OR REPLACE FUNCTION fn_crud_book(
-    IN InOption CHAR,
-    InBookId VARCHAR(36),
-    InTitle VARCHAR(200),
-    InDescription VARCHAR(500),
-    InWorkspace VARCHAR(50),
-    InIpReq VARCHAR(50),
-    InCategoryId VARCHAR(36)
-)
-    RETURNS TEXT
-    LANGUAGE plpgsql
-AS
+create function sp_crud_book(inoption character, inbookid character varying, intitle character varying, indescription character varying, inworkspace character varying, inipreq character varying, incategoryid character varying) returns character varying
+    language plpgsql
+as
 $$
 DECLARE
 LMessage VARCHAR(300) default '';
     LBookId  VARCHAR(36) default '';
 BEGIN
-    InTitle := TRIM(InTitle);
-    InDescription := TRIM(InDescription);
+BEGIN
+        InTitle := TRIM(InTitle);
+        InDescription := TRIM(InDescription);
 
-    IF (SELECT COUNT(*) FROM category where category_id = InCategoryId) = 0 THEN
-        LMessage := 'Esta categoria no existe o ha sido eliminado, por favor ingresar una valida';
+        IF (SELECT COUNT(*) FROM category where category_id = InCategoryId) = 0 THEN
+            LMessage := 'Esta categoria no existe o ha sido eliminado, por favor ingresar una valida';
 RAISE EXCEPTION '';
 END IF;
 
-    IF InOption = 'N' THEN
-        LBookId := uuid_generate_v4();
+        IF InOption = 'N' THEN
+            LBookId := uuid_generate_v4();
 INSERT INTO book(book_id, title, description, __workspace_create__, __workspace_update__, __create_date__,
                  __update_date__, __ip_req__, __deleted__)
 VALUES (LBookId, InTitle, InDescription, InWorkspace, InWorkspace, now(), now(), InIpReq, false);
 
-IF FOUND <> true THEN
-            LMessage := 'Ha Ocurrido un error al momento de insertar en la tabla book';
-            RAISE EXCEPTION '';
-end if;
-
 INSERT INTO book_detail(book_id, category_id) VALUES (LBookId, InCategoryId);
 
-IF FOUND <> true THEN
-            LMessage := 'Ha Ocurrido un error al momento de insertar en la tabla book_detail';
-            RAISE EXCEPTION '';
-end if;
-
-    ELSIF InOption = 'M' THEN
+ELSIF InOption = 'M' THEN
 UPDATE book
 SET title                = InTitle,
     description          = InDescription,
@@ -49,3 +31,34 @@ SET title                = InTitle,
     __update_date__      = now(),
     __ip_req__           = InIpReq
 WHERE book_id = InBookId
+  AND __deleted__ = false;
+
+UPDATE book_detail
+SET category_id = InCategoryId
+WHERE book_id = InBookId
+  AND __deleted__ = false;
+
+ELSIF InOption = 'D' THEN
+UPDATE book
+SET __deleted__          = true,
+    __workspace_update__ = InWorkspace,
+    __ip_req__           = InIpReq
+WHERE book_id = InBookId;
+
+UPDATE book_detail
+SET __deleted__ = true
+WHERE book_id = InBookId;
+
+END IF;
+
+COMMIT;
+END;
+
+EXCEPTION
+    WHEN OTHERS THEN ROLLBACK ;
+
+END;
+$$;
+
+alter function sp_crud_book(char, varchar, varchar, varchar, varchar, varchar, varchar) owner to postgres;
+
